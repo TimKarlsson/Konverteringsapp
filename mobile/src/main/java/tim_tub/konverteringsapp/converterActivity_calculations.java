631d43460ff166346a306683fa3f334b53914673
Tim Karlsson <tim_tub@hotmail.com>
2017-02-04 16:53:30 +0100

    Added a inch array to converter.

--- mobile/src/main/java/tim_tub/konverteringsapp/converterActivity_calculations.java
+++ mobile/src/main/java/tim_tub/konverteringsapp/converterActivity_calculations.java
@@ -1,5 +1,7 @@
 package tim_tub.konverteringsapp;
 
+import android.widget.Spinner;
+
 import java.math.BigDecimal;
 
 import tim_tub.common.converterMath;
@@ -12,15 +14,15 @@ import tim_tub.common.converterMath;
 
 class converterActivity_calculations {
 
-    static BigDecimal calculation(int iConvFrom, int iConvTo, BigDecimal result) {
+    static BigDecimal convertUnits(int iConvFrom, int iConvTo, BigDecimal bdResult) {
 
     //Effekt
         if (converterActivity_choise.convChoise == 1) {
             //HK <-> KW
             if (iConvFrom == 1 && iConvTo == 2) {
-                result = result.multiply(converterMath.hkTillKw);
+                bdResult = bdResult.multiply(converterMath.hkTillKw);
             } else if (iConvFrom == 2 && iConvTo == 1) {
-                result = result.multiply(converterMath.kwTillHk);
+                bdResult = bdResult.multiply(converterMath.kwTillHk);
             }
         }
 
@@ -28,9 +30,9 @@ class converterActivity_calculations {
         if (converterActivity_choise.convChoise == 2) {
             //Bar <-> Psi
             if (iConvFrom == 1 && iConvTo == 2) {
-                result = result.multiply(converterMath.barTillPsi);
+                bdResult = bdResult.multiply(converterMath.barTillPsi);
             } else if (iConvFrom == 2 && iConvTo == 1) {
-                result = result.multiply(converterMath.psiTillBar);
+                bdResult = bdResult.multiply(converterMath.psiTillBar);
             }
         }
 
@@ -38,40 +40,40 @@ class converterActivity_calculations {
         if (converterActivity_choise.convChoise == 4) {
             //dm<->cm
             if (iConvFrom == 1 && iConvTo == 2) {
-                result = result.multiply(converterMath.dmTillCm);
+                bdResult = bdResult.multiply(converterMath.dmTillCm);
             } else if (iConvFrom == 2 && iConvTo == 1) {
-                result = result.multiply(converterMath.cmTillDm);
+                bdResult = bdResult.multiply(converterMath.cmTillDm);
             }
             //dm<->mm
             if (iConvFrom == 1 && iConvTo == 3) {
-                result = result.multiply(converterMath.dmTillMm);
+                bdResult = bdResult.multiply(converterMath.dmTillMm);
             } else if (iConvFrom == 3 && iConvTo == 1) {
-                result = result.multiply(converterMath.mmTillDm);
+                bdResult = bdResult.multiply(converterMath.mmTillDm);
             }
             //dm<->Tum
             if (iConvFrom == 1 && iConvTo == 4) {
-                result = result.multiply(converterMath.dmTillTum);
+                bdResult = bdResult.multiply(converterMath.dmTillTum);
             } else if (iConvFrom == 4 && iConvTo == 1) {
-                result = result.multiply(converterMath.tumTillDm);
+                bdResult = bdResult.multiply(converterMath.tumTillDm);
             }
 
             // cm<->mm
             if (iConvFrom == 2 && iConvTo == 3) {
-                result = result.multiply(converterMath.cmTillMm);
+                bdResult = bdResult.multiply(converterMath.cmTillMm);
             } else if (iConvFrom == 3 && iConvTo == 2) {
-                result = result.multiply(converterMath.mmTillCm);
+                bdResult = bdResult.multiply(converterMath.mmTillCm);
             }
             // cm<->Tum
             if (iConvFrom == 2 && iConvTo == 4) {
-                result = result.multiply(converterMath.cmTillTum);
+                bdResult = bdResult.multiply(converterMath.cmTillTum);
             } else if (iConvFrom == 4 && iConvTo == 2) {
-                result = result.multiply(converterMath.tumTillCm);
+                bdResult = bdResult.multiply(converterMath.tumTillCm);
             }
             //mm<->Tum
             if (iConvFrom == 3 && iConvTo == 4) {
-                result = result.multiply(converterMath.mmTillTum);
+                bdResult = bdResult.multiply(converterMath.mmTillTum);
             } else if (iConvFrom == 4 && iConvTo == 3) {
-                result = result.multiply(converterMath.tumTillMm);
+                bdResult = bdResult.multiply(converterMath.tumTillMm);
             }
         }
 
@@ -79,48 +81,145 @@ class converterActivity_calculations {
         if (converterActivity_choise.convChoise == 3) {
             // Km/h <-> Mph
             if (iConvFrom == 1 && iConvTo == 2) {
-                result = result.multiply(converterMath.kmhTillMph);
+                bdResult = bdResult.multiply(converterMath.kmhTillMph);
             } else if (iConvFrom == 2 && iConvTo == 1) {
-                result = result.multiply(converterMath.mphTillKmh);
+                bdResult = bdResult.multiply(converterMath.mphTillKmh);
             }
             // Km/h <-> Knop
             if (iConvFrom == 1 && iConvTo == 3) {
-                result = result.multiply(converterMath.kmhTillKnop);
+                bdResult = bdResult.multiply(converterMath.kmhTillKnop);
             } else if (iConvFrom == 3 && iConvTo == 1) {
-                result = result.multiply(converterMath.knopTillKmh);
+                bdResult = bdResult.multiply(converterMath.knopTillKmh);
             }
             // Km/h <-> m/s
             if (iConvFrom == 1 && iConvTo == 4) {
-                result = result.multiply(converterMath.kmhTillMs);
+                bdResult = bdResult.multiply(converterMath.kmhTillMs);
             } else if (iConvFrom == 4 && iConvTo == 1) {
-                result = result.multiply(converterMath.msTillKmh);
+                bdResult = bdResult.multiply(converterMath.msTillKmh);
             }
 
             // Mph <-> Knop
             if (iConvFrom == 2 && iConvTo == 3) {
-                result = result.multiply(converterMath.mphTillKnop);
+                bdResult = bdResult.multiply(converterMath.mphTillKnop);
             } else if (iConvFrom == 3 && iConvTo == 2) {
-                result = result.multiply(converterMath.knopTillMph);
+                bdResult = bdResult.multiply(converterMath.knopTillMph);
             }
             // Mph <-> M/S
             if (iConvFrom == 2 && iConvTo == 4) {
-                result = result.multiply(converterMath.mphTillMs);
+                bdResult = bdResult.multiply(converterMath.mphTillMs);
             } else if (iConvFrom == 4 && iConvTo == 2) {
-                result = result.multiply(converterMath.msTillMph);
+                bdResult = bdResult.multiply(converterMath.msTillMph);
             }
             // M/S <-> Knop
             if (iConvFrom == 4 && iConvTo == 3) {
-                result = result.multiply(converterMath.msTillKnop);
+                bdResult = bdResult.multiply(converterMath.msTillKnop);
             } else if (iConvFrom == 3 && iConvTo == 4) {
-                result = result.multiply(converterMath.knopTillMs);
+                bdResult = bdResult.multiply(converterMath.knopTillMs);
             }
-
-
-
         }
+        return bdResult;
+    }
 
-
-        return result;
+    static String convertInch(Spinner spSpinnerInch, String sUserInput) {
+        switch (spSpinnerInch.getSelectedItemPosition()) {
+            case 1:
+                // 1/8"
+                sUserInput = "0.125";
+                break;
+            case 2:
+                // 3/16"
+                sUserInput = "0.1875";
+                break;
+            case 3:
+                // 1/4"
+                sUserInput = "0.25";
+                break;
+            case 4:
+                // 5/16"
+                sUserInput = "0.3125";
+                break;
+            case 5:
+                // 3/8"
+                sUserInput = "0.375";
+                break;
+            case 6:
+                // 7/16"
+                sUserInput = "0.4375";
+                break;
+            case 7:
+                // 1/2"
+                sUserInput = "0.5";
+                break;
+            case 8:
+                // 9/16"
+                sUserInput = "0.5625";
+                break;
+            case 9:
+                // 5/8"
+                sUserInput = "0.625";
+                break;
+            case 10:
+                // 3/4"
+                sUserInput = "0.75";
+                break;
+            case 11:
+                // 7/8"
+                sUserInput = "0.875";
+                break;
+            case 12:
+                // 1"
+                sUserInput = "1";
+                break;
+            case 13:
+                // 1 1/8"
+                sUserInput = "1.125";
+                break;
+            case 14:
+                // 1 1/4"
+                sUserInput = "1.25";
+                break;
+            case 15:
+                // 1 3/8"
+                sUserInput = "1.375";
+                break;
+            case 16:
+                // 1 1/2"
+                sUserInput = "1.5";
+                break;
+            case 17:
+                // 1 5/8"
+                sUserInput = "1.625";
+                break;
+            case 18:
+                // 1 3/4"
+                sUserInput = "1.75";
+                break;
+            case 19:
+                // 1 7/8"
+                sUserInput = "1.875";
+                break;
+            case 20:
+                // 2"
+                sUserInput = "2";
+                break;
+            case 21:
+                // 2 1/4"
+                sUserInput = "2.25";
+                break;
+            case 22:
+                // 2 1/2"
+                sUserInput = "2.5";
+                break;
+            case 23:
+                // 2 3/4"
+                sUserInput = "2.75";
+                break;
+            case 24:
+                // 3"
+                sUserInput = "3";
+                break;
+        }
+        return sUserInput;
     }
 
 }
--- mobile/src/main/java/tim_tub/konverteringsapp/converterActivity_core.java
+++ mobile/src/main/java/tim_tub/konverteringsapp/converterActivity_core.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -16,7 +17,7 @@ import android.widget.TextView;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class converterActivity_core extends AppCompatActivity {
+public class converterActivity_core extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     String error1 = "";
     String error2 = "";
@@ -29,7 +30,11 @@ public class converterActivity_core extends AppCompatActivity {
 
     int iConvFrom;
     int iConvTo;
-    String sConvTo;
+    String sConvTo = "";
+    String sUserInput;
+    BigDecimal bdResult;
+
+//----------------------------------------------------------------------------------------
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,10 +42,12 @@ public class converterActivity_core extends AppCompatActivity {
         setContentView(R.layout.activity_converter_core);
         ArrayAdapter<CharSequence> spinnerArrayAdapter = null;
 
+
     //Spinner1
         Spinner spinner1 = (Spinner) findViewById(R.id.convCoreSpinner1);
     //Spinner2
         Spinner spinner2 = (Spinner) findViewById(R.id.convCoreSpinner2);
+        Spinner spinnerInch = (Spinner) findViewById(R.id.convCoreSpinner3Inch);
     //Kollar vilka enheter som skall laddas till spinner
         if (converterActivity_choise.convChoise == 1) {
             spinnerArrayAdapter = ArrayAdapter.createFromResource(this, R.array.convPowerArray, android.R.layout.simple_spinner_item);
@@ -50,69 +57,126 @@ public class converterActivity_core extends AppCompatActivity {
             spinnerArrayAdapter = ArrayAdapter.createFromResource(this, R.array.convSpeedArray, android.R.layout.simple_spinner_item);
         } else if (converterActivity_choise.convChoise == 4) {
             spinnerArrayAdapter = ArrayAdapter.createFromResource(this, R.array.convMeasureArray, android.R.layout.simple_spinner_item);
+
+            spinner1.setOnItemSelectedListener(this);
+            spinner2.setOnItemSelectedListener(this);
+            spinnerInch.setOnItemSelectedListener(this);
         } else if (converterActivity_choise.convChoise == 5) {
             spinnerArrayAdapter = ArrayAdapter.createFromResource(this, R.array.convFlowArray, android.R.layout.simple_spinner_item);
         }
+
     //Anger valen för båda spinner
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(spinnerArrayAdapter);
         spinner2.setAdapter(spinnerArrayAdapter);
+    }
+
+//----------------------------------------------------------------------------------------
+
+
+//Körs när spinners ändras
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+    //Visa/Dölj showInchSpinner
+        if (converterActivity_choise.convChoise == 4 && (parent.getId()==R.id.convCoreSpinner1 || parent.getId()==R.id.convCoreSpinner2)) {
+            hideKeyboard(view);
+            showInchSpinner();
+        }
+        if (parent.getId()==R.id.convCoreSpinner3Inch && parent.getSelectedItemPosition() != 0) {
+            EditText etInput = (EditText) findViewById(R.id.convCoreInput);
+            etInput.setText("");
+            runConv(view);
+        }
+    }
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
 
+//----------------------------------------------------------------------------------------
 
-//Körs när man tryckt på knapp convCoreRun "Omvandla"
+//Visar eller döljer convInchSpinner och convInchInfo TextView.
+    void showInchSpinner() {
+        if (converterActivity_choise.convChoise == 4) {
+            Spinner spinnerInch = (Spinner) findViewById(R.id.convCoreSpinner3Inch);
+            Spinner spinner1 = (Spinner) findViewById(R.id.convCoreSpinner1);
+            Spinner spinner2 = (Spinner) findViewById(R.id.convCoreSpinner2);
+            TextView tvInfoInch = (TextView) findViewById(R.id.convCoreInchInfo);
+            TextView tv2 = (TextView) findViewById(R.id.convCoreText2);
+            ArrayAdapter<CharSequence> spinnerInchArrayAdapter = ArrayAdapter.createFromResource(this, R.array.convInchArray, android.R.layout.simple_spinner_item);
+            spinnerInchArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinnerInch.setAdapter(spinnerInchArrayAdapter);
+
+            if (spinnerInch.getVisibility() == View.INVISIBLE) {
+                if (tv2.getText().equals(getString(R.string.convCoreFrån)) && spinner1.getSelectedItemPosition() == 4 && spinner2.getSelectedItemPosition() != 0) {
+                    tvInfoInch.setVisibility(View.VISIBLE);
+                    spinnerInch.setVisibility(View.VISIBLE);
+                } else if (tv2.getText().equals(getString(R.string.convCoreTill)) && spinner2.getSelectedItemPosition() == 4 && spinner1.getSelectedItemPosition() != 0) {
+                    tvInfoInch.setVisibility(View.VISIBLE);
+                    spinnerInch.setVisibility(View.VISIBLE);
+                }
+            } else if (spinnerInch.getVisibility() == View.VISIBLE &&
+                    (tv2.getText().equals(getString(R.string.convCoreFrån)) && spinner1.getSelectedItemPosition() != 4) ||
+                    (tv2.getText().equals(getString(R.string.convCoreTill)) && spinner2.getSelectedItemPosition() != 4)){
+                tvInfoInch.setVisibility(View.INVISIBLE);
+                spinnerInch.setVisibility(View.INVISIBLE);
+                //TODO: Fortsätt implementera Tum listan! Utför beräkning på listan.
+            }
+        }
+
+    }
+
+//----------------------------------------------------------------------------------------
+
+//runConv körs när man tryckt på knapp convCoreRun "Omvandla"
+    //TODO: Lägg till långtryck för att konvertera direkt
     public void runConv(View view) {
-        TextView tvResultat = (TextView) findViewById(R.id.convCoreResultat);
+        TextView tvResult = (TextView) findViewById(R.id.convCoreResultat);
         TextView tv2 = (TextView) findViewById(R.id.convCoreText2);
         EditText etInput = (EditText) findViewById(R.id.convCoreInput);
         Spinner sp1 = (Spinner) findViewById(R.id.convCoreSpinner1);
         Spinner sp2 = (Spinner) findViewById(R.id.convCoreSpinner2);
+        Spinner spinnerInch = (Spinner) findViewById(R.id.convCoreSpinner3Inch);
+        sUserInput = "";
 
         String sp1String = sp1.getSelectedItem().toString();
         String sp2String = sp2.getSelectedItem().toString();
-        String userInput = etInput.getText().toString();
 
-        //Kollar efter fält som inte är angivna
-        checkForErrors(userInput, sp1String, sp2String, tv2);
-
-
-//Kollar så att inga fel är satta och sen utför konverteringen.
-        if ((!bErr1) && (!bErr2) && (!bErr3) && (!bErr4)) {
-            tvResultat.setTextColor(Color.parseColor("#000000"));
-            BigDecimal result = new BigDecimal(userInput);
-
-    // Vänder ordningen på sp1 och sp2 ifall konv. riktningen är bytt.
-//TODO: Flytta till changeConvDir??
-            if (tv2.getText().equals(getString(R.string.convCoreFrån))) {
-                iConvFrom = sp1.getSelectedItemPosition();
-                iConvTo = sp2.getSelectedItemPosition();
-                sConvTo = sp2String;
-            } else {
-                iConvFrom = sp2.getSelectedItemPosition();
-                iConvTo = sp1.getSelectedItemPosition();
-                sConvTo = sp1String;
-            }
-
-        //Kör begärd uträkning.
-            result = converterActivity_calculations.calculation(iConvFrom, iConvTo, result);
-        //Minskar till 3 decimaler och avrundar.
-            result = result.setScale(3, RoundingMode.HALF_UP);
-//TODO: Minska antalet decimaler om resultatet är *.000
-        //Skriver ut resultatet i convCoreResultat.
-            tvResultat.setText(String.valueOf(result) + " " + sConvTo);
+        if (spinnerInch.getVisibility() == View.VISIBLE && spinnerInch.getSelectedItemPosition() != 0) {
+            sUserInput = converterActivity_calculations.convertInch(spinnerInch, sUserInput);
         } else {
-            tvResultat.setTextColor(Color.parseColor("#FF0000"));
-            tvResultat.setText(error1 + error2 + error3 + error4);
+            sUserInput = etInput.getText().toString();
+        }
+        if (!sUserInput.equals("")) {
+            bdResult = new BigDecimal(sUserInput);
+        }
+
+
+    //Kollar efter fält som inte är angivna
+        checkForErrors(sp1String, sp2String, tv2);
+
+
+    //Kollar så att inga fel är satta och sen utför konverteringen.
+        if ((!bErr1) && (!bErr2) && (!bErr3) && (!bErr4)) {
+
+
+        //Kollar konverteringsriktningen och enheter
+            setConvFromAndTo(tv2, sp1, sp2);
+
+        //Konverterar och skriver ut resultatet
+            printResult(bdResult);
+        } else {
+        //Visar eventuella felmeddelanden
+            tvResult.setTextColor(Color.parseColor("#FF0000"));
+            tvResult.setText(error1 + error2 + error3 + error4);
         }
 
     //Gömmer keyboard
         hideKeyboard(view);
-    //Scrollar ned sidan vid behov för att visa resultatet
+    //Scrollar ned sidan till botten vid behov
         scrollDown();
     }
 
+//----------------------------------------------------------------------------------------
 
 //Körs när man trycker på pilknappen. Ändra riktning på omvandlingen.
     public void changeConvDir(View view) {
@@ -132,12 +196,14 @@ public class converterActivity_core extends AppCompatActivity {
             tv3.setText(R.string.convCoreTill);
         }
 //TODO: Kör inte om fel finns.
-        runConv(view);
+        showInchSpinner();
+        //runConv(view);
     }
 
+//----------------------------------------------------------------------------------------
 
 //Felhantering
-    void checkForErrors(String userInput, String sp1String, String sp2String, TextView tv2) {
+    void checkForErrors(String sp1String, String sp2String, TextView tv2) {
         //Nollar alla fel
         error1 = "";
         error2 = "";
@@ -149,7 +215,7 @@ public class converterActivity_core extends AppCompatActivity {
         bErr4 = false;
 
     //Err1 - Kollar om fält 1 är tomt.
-        if (userInput.equals("")) {
+        if (sUserInput.equals("")) {
             error1 = getString(R.string.convCoreErr1) + "\n";
             bErr1 = true;
         }
@@ -181,12 +247,33 @@ public class converterActivity_core extends AppCompatActivity {
         }
     }
 
+//----------------------------------------------------------------------------------------
+
+    void setConvFromAndTo (TextView tv2, Spinner sp1, Spinner sp2) {
+        // Vänder ordningen på sp1 och sp2 ifall konv. riktningen är bytt.
+//TODO: Flytta till changeConvDir??
+        if (tv2.getText().equals(getString(R.string.convCoreFrån))) {
+            iConvFrom = sp1.getSelectedItemPosition();
+            iConvTo = sp2.getSelectedItemPosition();
+            sConvTo = sp2.getSelectedItem().toString();
+        } else {
+            iConvFrom = sp2.getSelectedItemPosition();
+            iConvTo = sp1.getSelectedItemPosition();
+            sConvTo = sp1.getSelectedItem().toString();
+        }
+    }
+
+//----------------------------------------------------------------------------------------
 
 //Döljer knappsatsen.
     void hideKeyboard(View view) {
-        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        try {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        } catch (Exception e){        }
     }
+
+//----------------------------------------------------------------------------------------
 
 //Scrollar ned till botten av sidan.
     void scrollDown() {
@@ -197,4 +284,22 @@ public class converterActivity_core extends AppCompatActivity {
         }, 100L);
     }
 
+//----------------------------------------------------------------------------------------
+
+//Skriver ut resultatet
+    void printResult(BigDecimal bdResult) {
+        TextView tvResult = (TextView) findViewById(R.id.convCoreResultat);
+    //Kör begärd uträkning.
+        bdResult = converterActivity_calculations.convertUnits(iConvFrom, iConvTo, bdResult);
+    //Minskar till 3 decimaler och avrundar.
+        bdResult = bdResult.setScale(3, RoundingMode.HALF_UP);
+//TODO: Minska antalet decimaler om resultatet är *.000
+    //Skriver ut resultatet i convCoreResultat.
+        tvResult.setTextColor(Color.parseColor("#000000"));
+        tvResult.setText(String.valueOf(bdResult) + " " + sConvTo);
+    }
 }
+
+//Buggar
+//TODO: När "Till:" spinnern väljs så nollas spinnerInch!
+//TODO: Dölj tangentbord när man gått från EditText till Spinner
